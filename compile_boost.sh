0ba0019a12afdb21801d27dde258517add7e7691
yyq <1073079979@qq.com>
2016-12-10 21:33:43 +0800

    compile

---
+++ compile_boost.sh
@@ -0,0 +1,3 @@
+g++ hello.cpp  -I /home/yaoyongqiang/install/boost/include \
+-I /home/yaoyongqiang/anaconda2/include/python2.7 \
+-L /home/yaoyongqiang/install/boost/lib -lboost_python -shared -fPIC -o detect.so
